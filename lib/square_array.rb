b1de067b46301aca57a930649be4f65c1b41f945
George Blackmore <gtblackmore@gmail.com>
2019-12-08 19:42:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,13 @@
-def square_array(numbers)
-  numbers = [1,2,3]
-  while numbers.length <=3 do
-    a
+def square_array(array)
+  counter = 0 
+  new_array = [] 
+  
+    while array[counter] do
+      new_element = array[counter] ** 2
+      new_array.push(new_element)
+      
+      counter += 1 
+    end  
+    
+  return new_array
 end
